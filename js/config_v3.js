cb5da96fdd2a93d177e299413464caa8f4226abd
lynglaxco-cc <lyn.glaxco@cometchat.com>
2026-08-12 18:15:53 +0530

    Point Trivia v3 to new Apps Script deployment

--- js/config_v3.js
+++ js/config_v3.js
@@ -1,5 +1,5 @@
 window.TRIVIA_CONFIG_V3 = {
-  appScriptUrl: 'https://script.google.com/macros/s/AKfycbxCKHbQz8RfSgDz54rKWqXlW5T4ZiHOLSjCRZdgiBZyCy0BK6LKIbIgy5qjM-uo16d2/exec',
+  appScriptUrl: 'https://script.google.com/macros/s/AKfycby-OXW6SZzj1Yl0tVABQkdRS2HGHQh6-h3bgB40aOB5JO37pMWrK1kxq-pnfwsz2D5y/exec',
   questionCount: 20,
   cameraRequired: true,
   cameraMonitoring: true,
