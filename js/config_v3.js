474d2a3c84893c1da3d2b4c382aadcc812bc87d3
lynglaxco-cc <lyn.glaxco@cometchat.com>
2026-08-12 18:07:26 +0530

    Add Trivia frontend config v3

---
+++ js/config_v3.js
@@ -0,0 +1,8 @@
+window.TRIVIA_CONFIG_V3 = {
+  appScriptUrl: 'https://script.google.com/macros/s/AKfycbxCKHbQz8RfSgDz54rKWqXlW5T4ZiHOLSjCRZdgiBZyCy0BK6LKIbIgy5qjM-uo16d2/exec',
+  questionCount: 20,
+  cameraRequired: true,
+  cameraMonitoring: true,
+  fullscreenRequired: false,
+  version: '3.0'
+};
