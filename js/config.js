d3e1de57b6f8aeff7dd569768f460cb0098f4bcb
lynglaxco-cc <lyn.glaxco@cometchat.com>
2026-08-12 16:20:55 +0530

    Add frontend configuration

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// Frontend configuration only. Never place secrets in this file.
+window.TRIVIA_CONFIG = {
+  appScriptUrl: '',
+  questionTimeSeconds: 15,
+  cameraRequired: true,
+  fullscreenRequired: true,
+  integrityEnabled: true,
+  maxNameLength: 100
+};
